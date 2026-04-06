f241ebfe90e5202ebf95ecaf423e08f8112a0463
regencode <thomgozalie@gmail.com>
2026-04-06 23:51:56 +0700

    feat: reviews/ and products/ endpoint

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { ProductsRepository } from './products.repository';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [ProductsService, ProductsRepository],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
