d672c23c1dacfefe35af992cfae611984b8d21f2
regencode <thomgozalie@gmail.com>
2026-04-14 10:50:18 +0700

    feat: fix DATABASE_URL env loading

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+    const config = new DocumentBuilder()
     .setTitle('WeRent Backend API')
     .setDescription('API Product & Reviews Documentation')
     .setVersion('1.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
