14becba6ab95b3b748776b0499d85fcfca48b51a
regencode <thomgozalie@gmail.com>
2026-04-16 10:27:47 +0700

    feat: revert

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
+
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -8,6 +9,6 @@ export default defineConfig({
     seed: 'npx tsx ./prisma/seed.ts'
   },
   datasource: {
-    url: env('DATABASE_URL'), // Prisma uses this for the CLI and seeding
+    url: process.env.DATABASE_URL // Prisma uses this for the CLI and seeding
   },
 });
